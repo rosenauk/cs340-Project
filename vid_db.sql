8947fb8813f0f93a97a55de9eae7550484adc540
rosenauk <56204294+rosenauk@users.noreply.github.com>
2020-11-16 12:49:03 -0800

    Add files via upload

--- vid_db.sql
+++ vid_db.sql
@@ -61,7 +61,7 @@ CREATE TABLE `Platforms` (
 
 LOCK TABLES `Platforms` WRITE;
 /*!40000 ALTER TABLE `Platforms` DISABLE KEYS */;
-INSERT INTO `Platforms` VALUES (1,'Xbox One'),(2,'PlayStation 4'),(3,'Nintendo Switch');
+INSERT INTO `Platforms` VALUES (1,'Xbox One'),(2,'PlayStation 4'),(3,'Nintendo Switch'),(4,'PC');
 /*!40000 ALTER TABLE `Platforms` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,6 +92,52 @@ LOCK TABLES `PlatToVids` WRITE;
 /*!40000 ALTER TABLE `PlatToVids` ENABLE KEYS */;
 UNLOCK TABLES;
 
+
+
+
+/*
+LOCK TABLES `Videogames` WRITE;
+INSERT INTO `Videogames` 
+VALUES (1),(2),(3);
+UNLOCK TABLES;
+*/
+
+-- Publishers
+
+DROP TABLE IF EXISTS `Publishers`;
+CREATE TABLE `Publishers` (
+  `publisherID` int NOT NULL AUTO_INCREMENT,
+  `titleID` int NOT NULL,
+  `pName` varchar(100) NOT NULL,
+  CONSTRAINT `unique_pName` UNIQUE (`pName`),
+  PRIMARY KEY (`publisherID`),
+  Key `titleID` (`titleID`),
+  CONSTRAINT `publishers_titleID_fk` FOREIGN KEY (`titleID`) REFERENCES `Videogames` (`titleID`)
+);
+
+
+LOCK TABLES `Publishers` WRITE;
+INSERT INTO `Publishers` 
+VALUES (1, 3, 'Ubisoft'),(2, 2, 'Microsoft'),(3, 1, 'FROM SOFTWARE');
+UNLOCK TABLES;
+
+
+-- Ratings
+DROP TABLE IF EXISTS `Ratings`;
+CREATE TABLE `Ratings` (
+  `ratingID` int NOT NULL AUTO_INCREMENT,
+  `titleID` int,
+  `rating` varchar(100) NOT NULL,
+  PRIMARY KEY (`ratingID`),
+  Key `titleID` (`titleID`),
+  CONSTRAINT `ratings_titleID_fk` FOREIGN KEY (`titleID`) REFERENCES `Videogames` (`titleID`)
+);
+
+
+LOCK TABLES `Ratings` WRITE;
+INSERT INTO `Ratings` 
+VALUES (1, 2, 'great game'), (2,1, 'kinda sucked'), (3, 3, 'amazin');
+UNLOCK TABLES;
 
 
 
