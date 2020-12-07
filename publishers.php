16365855ae4b0815d48918ebeff891f8aad92b2f
Increnus <hernafra@oregonstate.edu>
2020-12-07 15:24:42 -0800

    publisher.php update functionality added.

--- publishers.php
+++ publishers.php
@@ -50,6 +50,28 @@
 <body>
 
 <?php
+		include 'dbhost.php';
+
+		if(isset($_POST['UpdateButton'])){
+			$u_publisherID     = $_POST['UpdateButton'];
+			$u_pName	       = $_POST['U_pName'];
+	
+			$u_sql = mysql_query("UPDATE Publishers SET pName='".$u_pName."' WHERE publisherID=$u_publisherID");
+			//echo $usql;
+	
+			if($u_sql){
+				$u_sql = null;
+			}
+			else{
+				echo "error updating record: " . $connector->error;
+			}
+			mysql_close($connector);
+		}
+	?>
+
+
+<?php
+		include 'dbhost.php';
 		//execute the SQL query and return records
 		$result = mysql_query("SELECT * FROM Publishers ORDER BY publisherID ASC");
 		?>
@@ -269,14 +291,17 @@
 			<th> </th>
 		  </tr>
 		  <?php
+		  	include 'dbhost.php';
             while ($row = mysql_fetch_assoc($result)) {
                 echo
-                "<tr>
+                "<form action='publishers.php' method='POST'><tr>
 					<td>{$row['publisherID']}</td>
-					<td>{$row['pName']}</td>
-					<td><button>Edit</button></td>
-				</tr>";
-            }
+					<td><input type='text' name='U_pName' value='" . $row['pName'] . "'</td>";
+				echo '<td><button class="update-button" name="UpdateButton'
+				.'" value="' . $row['publisherID'] . '"/>Update</td></button>';
+				echo '<td></form></td>';
+				echo "</tr>";
+			}
             ?>
 		</table>
 	</p>
